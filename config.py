b054ff8bd5ebcab4de116815bf06162260d645a9
Moses <moses.junior@student.moringaschool.com>
2021-09-30 11:42:35 +0300

    adding a qoute feature

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
     
 
     # email configurations
