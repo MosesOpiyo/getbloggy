4b17eb073edc247f82e1498dcb610aba8015ac43
Moses <moses.junior@student.moringaschool.com>
2021-09-30 12:19:37 +0300

    addition of a migrations folder

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
 
     # email configurations
